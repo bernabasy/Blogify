0ee5afdb7eabee86c4be6fc2010971129e4d635f
bernabasy <104406349+bernabasy@users.noreply.github.com>
2023-04-11 23:06:03 +0300

    Add seed for test

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+# db/seeds.rb
+
+# Create users
+5.times do |i|
+    user = User.create!(name: "User #{i+1}", photo: "https://picsum.photos/200", 
+                        bio: "I'm a user in this app.", posts_counter: 0)
+  
+    # Create posts for the current user
+    rand(1..5).times do |j|
+      post = Post.create!(title: "Post #{j+1} by #{user.name}",
+                          text: "This is post #{j+1} by #{user.name}.",
+                          comments_counter: 0, likes_counter: 0,
+                          author_id: user.id)
+            
+      # Update counters for the current post
+      Post.update_counters(post.id, comments_counter: post.comments.count, likes_counter: post.likes.count)
+    end
+  
+    # Update counters for the current user
+    User.update_counters(user.id, posts_counter: user.posts.count)
+  end
