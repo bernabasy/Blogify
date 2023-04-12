f958a390a81f68bfd738687d144f6b4fa406f41b
bernabasy <104406349+bernabasy@users.noreply.github.com>
2023-04-12 22:13:24 +0300

    Update PostsController with data

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,12 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @user = User.find(params[:user_id])
+    @posts = Post.where(author_id: @user.id)
   end
 
   def show
     @post = Post.find(params[:id])
+    @author = User.find(params[:user_id])
+    @comments = Comment.where(post_id: params[:id])
   end
 end
