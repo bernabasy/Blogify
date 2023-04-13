26ecd58103af0fd379d2095c97b96381e60bb7eb
bernabasy <104406349+bernabasy@users.noreply.github.com>
2023-04-13 21:44:21 +0300

    Update recent posts and comments from  desc to asc

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,7 +10,7 @@ class Post < ApplicationRecord
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def recent_comments
-    comments.order(created_at: :desc).limit(5)
+    comments.order(created_at: :asc).limit(5)
   end
 
   private
