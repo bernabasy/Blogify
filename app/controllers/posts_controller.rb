8ad745847a9cb91cdb3b3f6507f79ec89a1dbdb2
bernabasy <104406349+bernabasy@users.noreply.github.com>
2023-04-15 08:35:12 +0300

    Refactor PostsController and add create method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,43 @@
 class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    @posts = Post.where(author_id: @user.id)
+    @posts = Post.where(author_id: params[:user_id]).order(created_at: :desc)
   end
 
   def show
-    @post = Post.find(params[:id])
-    @author = User.find(params[:user_id])
-    @comments = Comment.where(post_id: params[:id])
+    @user = User.find(params[:user_id])
+    @post = Post.where(author_id: params[:user_id]).find(params[:id])
+    comment = Comment.new
+    like = Like.new
+    respond_to do |format|
+      format.html { render :show, locals: { comment:, like: } }
+    end
+  end
+
+  def new
+    new_post = Post.new
+    respond_to do |format|
+      format.html { render :new, locals: { post: new_post } }
+    end
+  end
+
+  def create
+    post_params = params.require(:new_post).permit(:title, :text)
+    post = Post.new(post_params)
+    post.author = current_user
+    post.comments_counter = 0
+    post.likes_counter = 0
+    respond_to do |format|
+      format.html do
+        if post.save
+          flash[:notice] = 'Post created successfully'
+          redirect_to users_path
+        else
+          Rails.logger.error(post.errors.full_messages)
+          flash.now[:alert] = 'Post creation failed'
+          render :new, locals: { post: }
+        end
+      end
+    end
   end
 end
