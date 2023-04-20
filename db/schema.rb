b2933ff6168fd36ae79bb33f274b55cc2677eeec
bernabasy <104406349+bernabasy@users.noreply.github.com>
2023-04-20 10:35:00 +0300

    Update default values

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_18_193758) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_19_183347) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -51,7 +51,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_18_193758) do
     t.integer "posts_counter", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "posts_count", default: 0
+    t.integer "posts_count"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
