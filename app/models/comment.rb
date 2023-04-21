205da9e4dea8dba6ce6ff8f9c11d59a4b1141c34
bernabasy <104406349+bernabasy@users.noreply.github.com>
2023-04-21 11:40:33 +0300

    Update counter

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,10 +3,15 @@ class Comment < ApplicationRecord
   belongs_to :post
 
   after_save :update_counter
+  before_destroy :decrease_counter
 
   private
 
   def update_counter
     post.increment!(:comments_counter)
+  end
+
+  def decrease_counter
+    post.decrement!(:comments_counter)
   end
 end
