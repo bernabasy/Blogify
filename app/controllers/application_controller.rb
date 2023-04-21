6a747823da7bc16704122a097dbdc0ff0600265b
bernabasy <104406349+bernabasy@users.noreply.github.com>
2023-04-21 11:40:07 +0300

    Add rescue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
-
-  rescue_from CanCan::AccessDenied do | exception |
+  rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, alert: exception.message
   end
 
